d1126e7bdb7a7f0fec0078c8d007690c426986ed
Angel Jimenez <angeljimenezabreu2@gmail.com>
2024-07-19 20:20:04 -0400

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}'],
+	theme: {
+		extend: {
+			colors: {
+        'electric-lime': '#ccff00',
+        'lime-zest': '#d9ff00',
+        'vegan-mastermind': '#20cf46',
+        'acid-pops': '#41ea67',
+        'gray-fog': '#F1F5F9',
+      },
+      boxShadow: {
+        'header-element': 'inset 0 0 10px 2px #c7c7c779'
+      }
+		},
+	},
+	plugins: [],
+}
