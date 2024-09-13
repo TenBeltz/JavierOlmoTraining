80dba81b7bf2bf1458e9f27b07575483cd90aec9
Angel Jimenez <angeljimenezabreu2@gmail.com>
2024-09-12 20:06:56 -0400

    chore: implement hybrid rendering mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
+  output: "hybrid",
   adapter: node({
     mode: "standalone"
   })
